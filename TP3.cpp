002afca3d2cd64e5afc67fa500ef990fcfac90de
Olivier Nadeau <oliviernadeau2003@gmail.com>
2024-12-04 13:39:58 -0500

    .

---
+++ TP3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // system("chcp 65001 > NUL"); // Set l'encodeur a UTF-8 pour lire et écrire des charactère spéciaux (... > NUL permet de 'suprimer' l'output de la commande)
+    printf("Hello World");
+    return 0;
+}
---
+++ compile_and_run.bat
@@ -0,0 +1,2 @@
+g++ .\TP3.cpp ^
+ -o TP3 && start ./TP3.exe
